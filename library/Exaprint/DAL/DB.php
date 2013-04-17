3d8d0970bc90411778dc4e7ace86039f91256816
romainbessuges <romainbessuges@gmail.com>
2013-04-17 23:54:31 +0200

    Ajout de la DB

---
+++ library/Exaprint/DAL/DB.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Exaprint\DAL;
+
+
+use RBM\SqlQuery\AbstractQuery;
+use RBM\SqlQuery\Renderer\SqlServer;
+use RBM\Utils\Dsn;
+
+class DB extends \PDO
+{
+    protected static $_instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$_instance)) self::$_instance = new self();
+        return self::$_instance;
+    }
+
+    public function __construct()
+    {
+        $dsn = new Dsn(Dsn::DBLIB, [
+            "dbname"   => $_SERVER["dbname"],
+            "host"     => $_SERVER["dbhost"],
+            "port"     => $_SERVER["dbport"],
+            "encoding" => "UTF-8"
+        ]);
+
+        parent::__construct($dsn, $_SERVER["dbuser"], $_SERVER["dbpassword"]);
+
+        AbstractQuery::setDefaultRenderer(new SqlServer());
+    }
+}

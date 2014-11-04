bbd2bb82a421848261f182c34cf61339cc981c15
kevinexaprint <kevin@exaprint.fr>
2014-11-04 14:32:33 +0100

    Webservice : Creation commande Printbox (commit v2)

--- library/Exaprint/DAL/WS/CreationCommandePrintbox.php
+++ library/Exaprint/DAL/WS/CreationCommandePrintbox.php
@@ -1,14 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kevin
- * Date: 04/11/14
- * Time: 14:19
- */
 
 namespace Exaprint\DAL\WS;
 
 
-class CreationCommandePrintbox {
+class CreationCommandePrintbox extends WebServiceAbstract
+{
+    protected $_params = [
+        'pnIdCommande'      => 0,
+        'pnIdClientFinal'   => 0,
+        'pmMontantTTC'      => 0,
+        'pmMontantHT'       => 0,
+        'pmMontantTva'      => 0,
+        'pbProforma'        => false,
+    ];
 
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return "WS_nCreationCommandePrintbox";
+    }
+
+    public function pnIdCommande($value)
+    {
+        return $this->_setParam('pnIdCommande', $value, self::TYPE_INT);
+    }
+
+    public function pnIdClientFinal($value)
+    {
+        return $this->_setParam('pnIdClientFinal', $value, self::TYPE_INT);
+    }
+
+    public function pmMontantTTC($value)
+    {
+        return $this->_setParam('pmMontantTTC', $value, self::TYPE_FLOAT);
+    }
+
+    public function pmMontantHT($value)
+    {
+        return $this->_setParam('pmMontantHT', $value, self::TYPE_FLOAT);
+    }
+
+    public function pmMontantTva($value)
+    {
+        return $this->_setParam('pmMontantTva', $value, self::TYPE_FLOAT);
+    }
+
+    public function pbProforma($value)
+    {
+        return $this->_setParam('pbProforma', $value, self::TYPE_BOOL);
+    }
 } 

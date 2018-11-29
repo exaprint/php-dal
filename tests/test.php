a609b3518b46f9e6e84ddd741ad869040bb61b8a
Kévin Monteiro <keiro.dev@gmail.com>
2018-11-29 10:10:51 +0100

    Feature/fs 2026 - FS-2026 - Give an other try if connection to DB failed (L45 to 62) (#10)
    FS-2026 - Give an other try if connection to DB failed

--- tests/test.php
+++ tests/test.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../vendor/autoload.php';
-require '/Users/admin/Sites/env.php';
+use Exaprint\DAL\Env;
 
 $fraisPort = new \Exaprint\DAL\WS\FraisDePortCommande();
 $fraisPort->idAdresseLivraison(2300239);
@@ -9,7 +9,7 @@ $fraisPort->idProduit(184953);
 $fraisPort->poidsTotalCommande(2200);
 
 /** @var \Exaprint\DAL\WS\FraisDePortCommandeResult $fraisPortResult */
-$fraisPortResult = \Exaprint\DAL\WS\WebServiceClient::get('prod')->call($fraisPort);
+$fraisPortResult = \Exaprint\DAL\WS\WebServiceClient::get(Env::ENV_STAGE)->call($fraisPort);
 
 $frais = new \Exaprint\DAL\WS\CSV\Frais($fraisPortResult->montantFrais, \Exaprint\DAL\Frais\TypeFrais::TRANSPORT, 0);
 
